90e5ef0391c6b826ce53ca08fa2614c6e892780b
Safin-Ali <safin.ali.7205@gmail.com>
2023-06-05 00:40:02 +0600

    nextjs-initial-template

---
+++ src/app/(landing-page)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1
+      className={`md:d-full-flex h-screen style-component`}
+      >
+        Basic Template
+      </h1>
+    </main>
+  )
+};
