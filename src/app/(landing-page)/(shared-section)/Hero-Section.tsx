a457203a0df34f4e3582cd183e9b5c85e9c13ad8
Safin-Ali <safin.ali.7205@gmail.com>
2023-06-09 23:22:05 +0600

    two new css class utilities, social-nav implement hero-sec

--- src/app/(landing-page)/(shared-section)/Hero-Section.tsx
+++ src/app/(landing-page)/(shared-section)/Hero-Section.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image';
 import React from 'react';
 import MouseScrollAnim from '@/components/Mouse-Scroll-Anim/Mouse-Scroll-Anim';
 import MultiTextSlide from '@/components/Multi-Text-Slide/Multi-Text-Slide';
+import SocialNav from '@/components/AppBar/Social-Nav';
 
 export default function HeroSection() {
 
     return (
         <section className={ `customise-container` }>
-            <div className={`hero-section`}>
+            <div className={ `hero-section` }>
+
                 <div className={ `d-v-flex w-full` }>
 
                     {/* hero content area */ }
@@ -16,30 +18,37 @@ export default function HeroSection() {
                         <h1 className={ `hero-title` }>Hi, { getGreeting() }</h1>
                         <h2 className={ `hero-subtitle my-5 font-medium` }>
                             I&#39;m Safin Ali an <MultiTextSlide
-                            textArr={['Front-End-Developer','Instractor']}
+                                textArr={ ['Front-End-Developer', 'Instractor'] }
                             />
                         </h2>
 
-                        <p className={`my-5`}>
+                        <p className={ `my-5` }>
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis laudantium beatae dolore. Soluta, a autem, magni voluptates exercitationem similique harum non corrupti deleniti neque reprehenderit impedit ex. Laborum, provident veritatis!
                         </p>
                     </article>
 
                     {/* hero my image mockup */ }
                     <div className={ `basis-1/2` }>
-                        <div className={`w-full my-10 overflow-hidden`}>
-                        <Image
-                            src={ 'https://i.ibb.co/8MJBTHk/sa-fin-ali.jpg' }
-                            alt={ `safin-ali-photo` }
-                            width={ 350 }
-                            height={250}
-                            className={ `hero-image` }
-                        />
+                        <div className={ `w-full my-10 overflow-hidden` }>
+                            <Image
+                                src={ 'https://i.ibb.co/8MJBTHk/sa-fin-ali.jpg' }
+                                alt={ `safin-ali-photo` }
+                                width={ 350 }
+                                height={ 250 }
+                                className={ `hero-image` }
+                            />
                         </div>
                     </div>
                 </div>
+
+                {/* scroll down anim */}
                 <div className={ `mouse-scroll-anim-area` }>
-                    <MouseScrollAnim/>
+                    <MouseScrollAnim />
+                </div>
+
+                {/* social network communicate icon */ }
+                <div className={`social-network-icons position-v-center`}>
+                    <SocialNav/>
                 </div>
             </div>
         </section>
