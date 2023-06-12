a49cbcc736dd012b9b912e47af42cb58909453fb
Safin-Ali <safin.ali.7205@gmail.com>
2023-06-12 19:16:49 +0600

    about-card components created

---
+++ src/components/Card/About-Card.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import React from 'react';
+interface PropsType {
+    iconSRC: string,
+    title: string,
+    subtitle: string,
+    width?: number,
+    height?: number
+};
+
+export default function AboutCard({
+    title,
+    height= 35,
+    iconSRC,
+    subtitle,
+    width= 35
+}:PropsType) {
+
+    return (
+        <div className={`about-card-box`}>
+
+            <div>
+                <Image
+                    src={iconSRC}
+                    alt={`${title}-icon`}
+                    width={width}
+                    height={height}
+                    className={`mx-auto`}
+                />
+            </div>
+
+            <h5 className={`about-card-title`}>{title}</h5>
+            <h6 className={`about-card-subtitle`}>{subtitle}</h6>
+        </div>
+    );
+};
