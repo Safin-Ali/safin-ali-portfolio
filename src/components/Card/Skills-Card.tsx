5d5caa80c68431b3ce259184c48794881e4aff2f
Safin-Ali <safin.ali.7205@gmail.com>
2023-06-13 02:21:41 +0600

    skill-card component

---
+++ src/components/Card/Skills-Card.tsx
@@ -0,0 +1,48 @@
+'use client'
+import Image from 'next/image';
+import React, { useState } from 'react';
+import SimpleTooltip from '../Tooltip/Simple-Tooltip';
+interface PropsType {
+    iconSRC: string,
+    level: 'Intermediate' | 'Familiar' | 'Expert' | 'Master',
+    name: string
+};
+
+export default function SkillsCard({
+    iconSRC,
+    level,
+    name
+}: PropsType) {
+
+    const [toolBool, setTool] = useState<boolean>(false)
+
+    return (
+        <div onMouseEnter={ () => setTool(true) } onMouseLeave={ () => setTool(false) } className={ `skill-card-box` }>
+
+            {/* Skill Icon */ }
+            <div className={ `h-[35px]` }>
+                <Image
+                    src={ iconSRC }
+                    alt={ `${name}_icon` }
+                    width={ 100 }
+                    height={ 100 }
+                />
+            </div>
+
+            {/* Skill Level */ }
+            <div className={ `text-center` }>
+                <small className={ `text-neutral-500 font-medium` }>{ level }</small>
+            </div>
+
+            {
+                toolBool
+                &&
+                <SimpleTooltip style={ { top: '-53%' } }>
+                    {
+                        name
+                    }
+                </SimpleTooltip>
+            }
+        </div>
+    );
+};
---
+++ src/types/skillls-info-type.ts
@@ -0,0 +1,6 @@
+export interface SkillInfoType {
+    _id: string
+    icon:string,
+    name: string,
+    level: 'Intermediate' | 'Familiar' | 'Expert' | 'Master',
+}
