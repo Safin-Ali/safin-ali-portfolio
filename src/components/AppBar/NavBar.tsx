ebb0be738b3eb38884a3e46ef547b94cfcf55877
Safin-Ali <safin.ali.7205@gmail.com>
2023-07-06 23:48:03 +0600

    conditional rendering functionality implement

--- src/components/AppBar/NavBar.tsx
+++ src/components/AppBar/NavBar.tsx
@@ -1,55 +1,109 @@
 'use client'
 import navLinks from '@/data/nav-links';
+import useScreenWidth from '@/hooks/useScreenWidth';
 import Image from 'next/image';
 import NavLink from "next/link";
 import { Link } from 'react-scroll';
-interface PropsType { };
+import ResNavBar from './Res-NavBar';
+import { TbLayoutGridAdd } from 'react-icons/tb';
+import { BsUiChecksGrid } from 'react-icons/bs';
+import { useState } from 'react';
 
 export default function NavBar() {
 
+  const screenWidth = useScreenWidth();
+
+  const [resNav, setResNav] = useState<boolean>(false);
+
   return (
     <header>
-      <nav className={ `nav-container flex-v-center customise-container` }>
+      <nav
+        className={ `nav-container flex-v-center customise-container` }
+      >
 
         {/* Web Name */ }
-        <div>
-          <span className={`text-3xl font-mono`}>
-            <NavLink href={'/'}>
+        <div title={ 'Web Name' }>
+          <span className={ `text-3xl font-mono` }>
+            <NavLink href={ '/' }>
               <Image
-                src={'https://svgshare.com/i/uD_.svg'}
-                width={200}
-                height={200}
-                alt={'Safin-Logo'}
+                src={ 'https://svgshare.com/i/uD_.svg' }
+                width={ 200 }
+                height={ 200 }
+                alt={ 'Safin-Logo' }
               />
             </NavLink>
           </span>
         </div>
 
-        {/* Nav Items */ }
+        {
+          screenWidth >= 768
+            ?
+            // desktop screen nav
+            <>
 
-        <div className={ `nav-items-container` }>
-          <ul className={ `flex-full-center` }>
-            {
-              navLinks.map(
-                (link, idx) => <li
-                  key={ idx }
-                  aria-label={ `nav-link` }
-                >
-                  <Link
-                    className={ `nav-item` }
-                    activeClass={`nav-active`}
-                    spy
-                    to={ link.value }
-                  >
-                    {link.label
-                    }
-                  </Link>
+              {/* Nav Items */ }
 
-                </li>
-              )
-            }
-          </ul>
-        </div>
+              <div className={ `nav-items-container` }>
+                <ul className={ `flex-full-center` }>
+                  {
+                    navLinks.map(
+                      (link, idx) => <li
+                        key={ idx }
+                        aria-label={ `nav-link` }
+                      >
+                        <Link
+                          className={ `nav-item` }
+                          activeClass={ `nav-active` }
+                          spy
+                          to={ link.value }
+                        >
+                          { link.label
+                          }
+                        </Link>
+
+                      </li>
+                    )
+                  }
+                </ul>
+              </div>
+            </>
+            :
+            // responsive navbar
+            <>
+              {/* humburger icon */ }
+              <div
+                title={ 'Nav Grid Icon' }
+                className={ `${!resNav ? 'rotate-[180deg]' : 'bg-zinc-700'} cursor-pointer p-1.5 rounded-md` }
+                onClick={ () => setResNav(!resNav) }
+              >
+                {
+                  !resNav
+                    ?
+                    <TbLayoutGridAdd
+                      size={ 30 }
+                      className={ `anim-fade` }
+                    />
+                    :
+                    <BsUiChecksGrid
+                      size={ 24 }
+                      className={ `anim-fade` }
+                      color={ '#ffffff' }
+                    />
+                }
+
+              </div>
+
+              {
+                resNav
+                &&
+                <ResNavBar />
+              }
+
+
+
+            </>
+        }
+
       </nav>
     </header>
   );
