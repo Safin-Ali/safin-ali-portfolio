6ed80097405232a6c66904353736bbc98429b39d
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-10-03 00:48:41 +0600

    implement theme btn in navbar without handler

--- src/components/AppBar/NavBar.tsx
+++ src/components/AppBar/NavBar.tsx
@@ -9,108 +9,116 @@ import { TbLayoutGridAdd } from 'react-icons/tb';
 import { BsUiChecksGrid } from 'react-icons/bs';
 import { useState } from 'react';
 import useAOS from '@/hooks/useAOS';
+import ThemeToggle from '../Button/Theme-Toggle';
 
 export default function NavBar() {
 
-  const screenWidth = useScreenWidth();
+	const screenWidth = useScreenWidth();
 
-  const [resNav, setResNav] = useState<boolean>(false);
+	const [resNav, setResNav] = useState<boolean>(false);
 
-  // for aos animate on scroll
-  useAOS();
+	// for aos animate on scroll
+	useAOS();
 
-  return (
-    <header
-      data-aos="fade-down"
-    >
-      <nav
-        className={ `nav-container flex-v-center customise-container` }
-      >
+	return (
+		<header
+			data-aos="fade-down"
+		>
+			<nav
+				className={ `nav-container flex-v-center customise-container` }
+			>
 
-        {/* Web Name */ }
-        <div className={ `w-2/5 sm:w-3/5 md:w-full` } title={ 'Web Name (Safin)' }>
-          <span className={ `text-3xl font-mono` }>
-            <NavLink href={ '/' }>
-              <Image
-                src={ 'https://svgshare.com/i/uD_.svg' }
-                width={ 200 }
-                height={ 200 }
-                alt={ 'Safin-Logo' }
-              />
-            </NavLink>
-          </span>
-        </div>
+				{/* Web Name */ }
+				<div className={ `w-2/5 sm:w-3/5 md:w-full` } title={ 'Web Name (Safin)' }>
+					<span className={ `text-3xl font-mono` }>
+						<NavLink href={ '/' }>
+							<Image
+								src={ 'https://svgshare.com/i/uD_.svg' }
+								width={ 200 }
+								height={ 200 }
+								alt={ 'Safin-Logo' }
+							/>
+						</NavLink>
+					</span>
+				</div>
 
-        {
-          screenWidth >= 768
-            ?
-            // desktop screen nav
-            <>
+				{
+					screenWidth >= 768
+						?
+						// desktop screen nav
+						<>
 
-              {/* Nav Items */ }
+							{/* Nav Items */ }
 
-              <div className={ `nav-items-container` }>
-                <ul className={ `flex-full-center` }>
-                  {
-                    navLinks.map(
-                      (link, idx) => <li
-                        key={ idx }
-                        aria-label={ `nav-link` }
-                      >
-                        <Link
-                          className={ `nav-item` }
-                          activeClass={ `nav-active` }
-                          spy
-                          to={ link.value }
-                        >
-                          { link.label
-                          }
-                        </Link>
+							<div className={ `nav-items-container` }>
+								<ul className={ `flex-full-center` }>
+									{
+										navLinks.map(
+											(link, idx) => <li
+												key={ idx }
+												aria-label={ `nav-link` }
+											>
+												<Link
+													className={ `nav-item` }
+													activeClass={ `nav-active` }
+													spy
+													to={ link.value }
+												>
+													{ link.label
+													}
+												</Link>
 
-                      </li>
-                    )
-                  }
-                </ul>
-              </div>
-            </>
-            :
-            // responsive navbar
-            <>
-              {/* humburger icon */ }
-              <div
-                title={ 'Nav Grid Icon' }
-                className={ `${!resNav ? 'rotate-[180deg]' : 'bg-zinc-700'} cursor-pointer p-1.5 rounded-md` }
-                onClick={ () => setResNav(!resNav) }
-              >
-                {
-                  !resNav
-                    ?
-                    <TbLayoutGridAdd
-                      size={ 30 }
-                      className={ `anim-fade` }
-                    />
-                    :
-                    <BsUiChecksGrid
-                      size={ 24 }
-                      className={ `anim-fade` }
-                      color={ '#ffffff' }
-                    />
-                }
+											</li>
+										)
+									}
+								</ul>
+							</div>
 
-              </div>
+							<div className={ `mx-2 mb-1.5` }>
+								<ThemeToggle />
+							</div>
+						</>
+						:
+						// responsive navbar
+						<div className={`flex-full-center`}>
+							<div className={ `mx-2 mb-1.5` }>
+								<ThemeToggle />
+							</div>
+							{/* humburger icon */ }
+							<div
+								title={ 'Nav Grid Icon' }
+								className={ `${!resNav ? 'rotate-[180deg]' : 'bg-zinc-700'} cursor-pointer p-1.5 rounded-md` }
+								onClick={ () => setResNav(!resNav) }
+							>
+								{
+									!resNav
+										?
+										<TbLayoutGridAdd
+											size={ 30 }
+											className={ `anim-fade` }
+										/>
+										:
+										<BsUiChecksGrid
+											size={ 24 }
+											className={ `anim-fade` }
+											color={ '#ffffff' }
+										/>
+								}
 
-              {
-                <ResNavBar
-                  expandBool={ resNav }
-                />
-              }
+							</div>
+
+							{
+								<ResNavBar
+									expandBool={ resNav }
+								/>
+							}
 
 
 
-            </>
-        }
+						</div>
+				}
 
-      </nav>
-    </header>
-  );
+			</nav>
+		</header>
+	);
 };
