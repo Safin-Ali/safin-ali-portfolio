e6ec8a9e20dd78402ee5584371efae457861d86d
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-10-03 05:01:43 +0600

    fix theme toggle button css and added handler

--- src/components/Button/Theme-Toggle.tsx
+++ src/components/Button/Theme-Toggle.tsx
@@ -1,13 +1,54 @@
-import newArr from '@/utilities/new-arr';
-import React from 'react';
-interface PropsType { };
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
+import React, { useEffect, useState } from 'react';
+
+type themeLiteral = 'light' | 'dark'
 
 export default function ThemeToggle() {
+	const [isDark, setDark] = useState<boolean>(false);
+	const [toggle, setToggle] = useState<boolean>(false);
+
+	/**
+	 *
+	 * @param isDark
+	 * @param oppositeThemeVal
+	 * @param prefers
+	 */
+	const updateTheme = (isDark:boolean,oppositeThemeVal:themeLiteral,prefers:boolean = false) => {
+		const dom = document.documentElement.classList;
+		dom.replace(isDark ? 'light' : 'dark',oppositeThemeVal);
+		setDark(isDark);
+		if(!prefers) localStorage.setItem('theme', oppositeThemeVal);
+	};
+
+	useEffect(() => {
+		const systemTheme = matchMedia('(prefers-color-scheme: dark)').matches;
+		const curTheme = localStorage.getItem('theme');
+		const oppositeThemeVal = !isDark ? 'light' : 'dark';
+		console.log('sa');
+
+
+		if (!curTheme && !toggle) {
+			if (systemTheme) {
+				updateTheme(true,'dark',true);
+			}
+		} else if (!curTheme) {
+			updateTheme(isDark ? true : false,oppositeThemeVal);
+		} else if (curTheme === 'dark') {
+			updateTheme(false,'light');
+		} else {
+			updateTheme(true,'dark');
+		};
+	}, [toggle]);
 
 	return (
-			<label className="switch">
-				<input type="checkbox"/>
-					<span className="slider"></span>
-			</label>
+		<label className="switch">
+			<input onClick={ () => {
+				setDark(!isDark);
+				setToggle(!toggle);
+			} }
+				type="checkbox" />
+			<span className={ `slider ${isDark ? 'moon' : 'sun'}` }></span>
+		</label>
 	);
 };
