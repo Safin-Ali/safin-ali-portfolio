3c04beeb5d8e3ab6a490e28e7c98dac2b4360f82
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-10-03 04:39:27 +0600

    add default light theme in html tag and TWCSS config menual dark class enabled

--- src/app/(landing-page)/layout.tsx
+++ src/app/(landing-page)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`light`}>
       <link rel="icon" href="../favicon.ico" sizes="any" />
       <body className={inter.className}>
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors:{
-        "wh-base":`hsl(220 0% 98% / 1)`,
-        "wh-block":`#FBFCFD`,
-        "wh-hover":`hsl(210 10% 97% / 1)`,
-        "wh-menu-hover":`#E8EBF0`,
-        "wh-border":`#E2E5EA`,
-        "bl-base":`#1B1B1B`,
-        "bl-sub-title":`#666666`,
-        "primaryL":`hsl(217 2% 25% / 1)`,
-      },
-      spacing:{
-        'nav':'70px'
-      },
-      height:{
-        'nav': '70px',
-        'fit-screen': 'calc(100vh - 70px)',
-      },
-      boxShadow:{
-        'normal': '6px 10px 18px 0px',
-        'card': '2px 7px 10px 0px',
-        'square': '0px 0px 20px 15px',
-      }
-    },
-  },
-  plugins: [],
+	darkMode: 'class',
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				"wh-base": `hsl(220 0% 98% / 1)`,
+				"wh-block": `#FBFCFD`,
+				"wh-hover": `hsl(210 10% 97% / 1)`,
+				"wh-menu-hover": `#E8EBF0`,
+				"wh-border": `#E2E5EA`,
+				"bl-base": `#1B1B1B`,
+				"bl-sub-title": `#666666`,
+				"primaryL": `hsl(217 2% 25% / 1)`,
+			},
+			spacing: {
+				'nav': '70px'
+			},
+			height: {
+				'nav': '70px',
+				'fit-screen': 'calc(100vh - 70px)',
+			},
+			boxShadow: {
+				'normal': '6px 10px 18px 0px',
+				'card': '2px 7px 10px 0px',
+				'square': '0px 0px 20px 15px',
+			}
+		},
+	},
+	plugins: [],
 }
