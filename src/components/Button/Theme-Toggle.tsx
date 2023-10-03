28bbcfe54301ec6f21f6cb817b12b63ec690a5a7
Dev-SA-JS <safin.ali.devx@gmail.com>
2023-10-04 04:07:03 +0600

    used themeToggler hook for effectively render

--- src/components/Button/Theme-Toggle.tsx
+++ src/components/Button/Theme-Toggle.tsx
@@ -1,54 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client'
-import React, { useEffect, useState } from 'react';
+import useThemeToggler from '@/hooks/useThemeToggler';
+import React, { memo, useState } from 'react';
 
-type themeLiteral = 'light' | 'dark'
+export default memo(function ThemeToggle() {
 
-export default function ThemeToggle() {
-	const [isDark, setDark] = useState<boolean>(false);
-	const [toggle, setToggle] = useState<boolean>(false);
-
-	/**
-	 *
-	 * @param isDark
-	 * @param oppositeThemeVal
-	 * @param prefers
-	 */
-	const updateTheme = (isDark:boolean,oppositeThemeVal:themeLiteral,prefers:boolean = false) => {
-		const dom = document.documentElement.classList;
-		dom.replace(isDark ? 'light' : 'dark',oppositeThemeVal);
-		setDark(isDark);
-		if(!prefers) localStorage.setItem('theme', oppositeThemeVal);
+	const [isDark,setDark] = useState<boolean>(false);
+	const {theme,updateTheme} = useThemeToggler({
+		activeHandler:setDark
+	});
+	const handleToggleForStore = () => {
+		updateTheme(theme === 'dark' ? 'light' : 'dark',true)
 	};
-
-	useEffect(() => {
-		const systemTheme = matchMedia('(prefers-color-scheme: dark)').matches;
-		const curTheme = localStorage.getItem('theme');
-		const oppositeThemeVal = !isDark ? 'light' : 'dark';
-		console.log('sa');
-
-
-		if (!curTheme && !toggle) {
-			if (systemTheme) {
-				updateTheme(true,'dark',true);
-			}
-		} else if (!curTheme) {
-			updateTheme(isDark ? true : false,oppositeThemeVal);
-		} else if (curTheme === 'dark') {
-			updateTheme(false,'light');
-		} else {
-			updateTheme(true,'dark');
-		};
-	}, [toggle]);
-
 	return (
 		<label className="switch">
-			<input onClick={ () => {
-				setDark(!isDark);
-				setToggle(!toggle);
-			} }
+			<input onClick={handleToggleForStore}
 				type="checkbox" />
 			<span className={ `slider ${isDark ? 'moon' : 'sun'}` }></span>
 		</label>
 	);
-};
+});
