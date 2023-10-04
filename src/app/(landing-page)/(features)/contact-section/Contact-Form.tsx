9163fb2a13ea00f1cd01df431c446fc6a782960a
Dev-SA-JS <safin.ali.devx@gmail.com>
2023-10-05 02:54:59 +0600

    Contact-Form component submit button 3 type status and animation added

--- src/app/(landing-page)/(features)/contact-section/Contact-Form.tsx
+++ src/app/(landing-page)/(features)/contact-section/Contact-Form.tsx
@@ -1,76 +1,132 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import FloatingInputField from '@/components/Field/Floating-Input-Field';
 import FloatingTextAreaField from '@/components/Field/Floating-Textarea-Field';
 import PrimaryBtn from '@/components/Button/Primary-Btn';
 import { RiSendPlaneFill } from 'react-icons/ri';
+import ProcessingBtn, { AsyncTaskStatusType } from '@/components/Button/Processing-Btn';
 
 interface FormType extends HTMLFormControlsCollection {
-    subject: HTMLInputElement,
-    name: HTMLInputElement,
-    desc: HTMLInputElement,
-    email: HTMLInputElement
+	subject: HTMLInputElement,
+	name: HTMLInputElement,
+	desc: HTMLInputElement,
+	email: HTMLInputElement
 };
 
 interface CustomForm extends HTMLFormElement {
-    elements: FormType
+	elements: FormType
 };
+
+type InputFormType = React.FormEvent<CustomForm>;
+
+interface AnimButtonType {
+	reqeust: boolean,
+	status:"ready" | AsyncTaskStatusType
+}
 
 export default function ContactForm() {
 
-    const handleSendMessageForm = (e: React.FormEvent<CustomForm>): void => {
-        e.preventDefault();
-        const form = e.currentTarget.elements;
-    };
+	const [animBool, setAnimBool] = useState<AnimButtonType>({
+		reqeust: false,
+		status:'ready'
+	});
 
-    return (
-        <form className={`w-full`} onSubmit={ handleSendMessageForm }>
-            <FloatingInputField
-                type={ 'text' }
-                name={ `subject` }
-                className={ `bg-wh-block` }
-            >
-                Subject
-            </FloatingInputField>
+	const handleSendMessageForm = (e: InputFormType): void => {
+		e.preventDefault();
+		const form = e.currentTarget.elements;
+		setTimeout(() => {
+			setAnimBool({...animBool,status:'success'});
+			setTimeout(() => {
+				setAnimBool({reqeust:false,status:'ready'});
+			},1700)
+		},3000)
 
-            <FloatingInputField
-                type={ 'text' }
-                name={ `name` }
-                className={ `bg-wh-block` }
-            >
-                Your Name
-            </FloatingInputField>
+	};
 
-            <FloatingInputField
-                type={ 'email' }
-                name={ `email` }
-                className={ `bg-wh-block` }
-            >
-                Your Email
-            </FloatingInputField>
+	return (
+		<form className={ `w-full` } onSubmit={ (e: InputFormType) => {
+			handleSendMessageForm(e);
+			setAnimBool({...animBool,reqeust: true});
+		} }>
+			<FloatingInputField
+				type={ 'text' }
+				name={ `subject` }
+				className={ `bg-wh-block` }
+			>
+				Subject
+			</FloatingInputField>
 
-            <FloatingTextAreaField
-                className={ `bg-wh-block top-5 resize-none` }
-                rows={ 5 }
-                name={ `desc` }
+			<FloatingInputField
+				type={ 'text' }
+				name={ `name` }
+				className={ `bg-wh-block` }
+			>
+				Your Name
+			</FloatingInputField>
 
-            >
-                Message
-            </FloatingTextAreaField>
+			<FloatingInputField
+				type={ 'email' }
+				name={ `email` }
+				className={ `bg-wh-block` }
+			>
+				Your Email
+			</FloatingInputField>
 
-            <PrimaryBtn
-                className={ `px-5 py-2.5` }
-            >
-                <div className={ `flex-full-center gap-2` }>
-                    <div>Send Message</div>
-                    <div>
-                        <RiSendPlaneFill
-                            size={ 20 }
-                            className={ `mb-0.5` }
-                        />
-                    </div>
-                </div>
-            </PrimaryBtn>
-        </form>
-    );
+			<FloatingTextAreaField
+				className={ `bg-wh-block top-5 resize-none` }
+				rows={ 5 }
+				name={ `desc` }
+
+			>
+				Message
+			</FloatingTextAreaField>
+
+			<PrimaryBtn
+				className={ `px-5 py-2.5` }
+				disabled={animBool.status !== 'ready' ? true : false}
+			>
+				<div className={ `overflow-hidden` }>
+					{
+						animBool.status === 'ready'
+							?
+								/* initial button or send button */
+							<>
+								<div
+								className={ `email-send-btn flex-full-center ${animBool.reqeust ? '-translate-y-14' : ''}` }
+								onTransitionEnd={() => setAnimBool({ ...animBool, status: 'pending' })}
+								>
+									<div>Send message</div>
+									<div>
+										<RiSendPlaneFill
+											size={ 20 }
+											className={ `mb-0.5` }
+										/>
+									</div>
+								</div>
+							</>
+							:
+							<>
+								<div className={ `animate-slide-up pointer-events-none translate-y-14` }>
+									<ProcessingBtn
+										status={animBool.status}
+									>
+										{
+											animBool.status === 'pending'
+											?
+											'Sending'
+											:
+											animBool.status ==='success'
+											?
+											'Sent'
+											:
+											'Failed'
+										}
+									</ProcessingBtn>
+								</div>
+							</>
+					}
+				</div>
+			</PrimaryBtn>
+		</form>
+	);
 };
