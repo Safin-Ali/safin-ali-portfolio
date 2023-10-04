0b63c1ec957c73139b077d7ac3cdbdeb4d95d9f3
Dev-SA-JS <safin.ali.devx@gmail.com>
2023-10-04 17:57:51 +0600

    added integrated url based on contact card type

--- src/app/(landing-page)/(features)/contact-section/Contact-Method.tsx
+++ src/app/(landing-page)/(features)/contact-section/Contact-Method.tsx
@@ -3,27 +3,32 @@ import { LuLinkedin } from 'react-icons/lu';
 import { LiaFacebookMessenger } from 'react-icons/lia';
 import { FaWhatsapp } from 'react-icons/fa';
 import React from 'react';
-interface PropsType { };
 
 export default function ContactMethod() {
 
-    return (
-        <>
-            <ContactCard
-                path={ 'safin-ali' }
-                type={ 'Linkedin' }
-                icon={ <LuLinkedin size={ 27 } /> }
-            />
-            <ContactCard
-                path={ 'safin.ali.7205' }
-                type={ 'Messenger' }
-                icon={ <LiaFacebookMessenger size={ 30 } /> }
-            />
-            <ContactCard
-                path={ '+880 1316987205' }
-                type={ 'WhatsApp' }
-                icon={ <FaWhatsapp size={ 25 } /> }
-            />
-        </>
-    );
+	const myNumber = '+8801316987205';
+
+
+	return (
+		<>
+			<ContactCard
+				path={ 'safin-ali' }
+				url={ 'https://www.linkedin.com/in/safin-ali/' }
+				type={ 'Linkedin' }
+				icon={ <LuLinkedin size={ 27 } /> }
+			/>
+			<ContactCard
+				path={ 'safin.ali.7205' }
+				url={ 'http://m.me/safin.ali.7205' }
+				type={ 'Messenger' }
+				icon={ <LiaFacebookMessenger size={ 30 } /> }
+			/>
+			<ContactCard
+				path={ myNumber }
+				url={ `https://web.whatsapp.com/send?phone=${myNumber}` }
+				type={ 'WhatsApp' }
+				icon={ <FaWhatsapp size={ 25 } /> }
+			/>
+		</>
+	);
 };
--- src/components/Card/Contact-Card.tsx
+++ src/components/Card/Contact-Card.tsx
@@ -1,42 +1,47 @@
 import React from 'react';
-import {PiNavigationArrowThin} from 'react-icons/pi';
+import { PiNavigationArrowThin } from 'react-icons/pi';
 
 
 interface PropsType {
-    type: string,
-    path: string,
-    icon: React.ReactElement
+	type: string,
+	url: string,
+	path: string,
+	icon: React.ReactElement
 };
 
 export default function ContactCard({
-    path,
-    type,
-    icon
+	path,
+	url,
+	type,
+	icon
 }: PropsType) {
 
-    return (
-        <div className={ `contact-card` }>
+	return (
+		<div
+			className={ `contact-card` }
+		>
+			<a href={ url } target={ '_blank' }>
+				<div className={ `flex-full-center` }>
+					{
+						icon
+					}
+				</div>
 
-            <div className={`flex-full-center`}>
-                {
-                    icon
-                }
-            </div>
+				<h3 className={ `capitalize basis-1/2` }>{ type }</h3>
 
-            <h3 className={ `capitalize basis-1/2` }>{ type }</h3>
+				<h5><strong className={ `font-semibold` }>{ path }</strong></h5>
 
-            <h5><strong className={`font-semibold`}>{ path }</strong></h5>
+				<div className={ `flex-full-center gap-2` }>
+					<h6>Contact</h6>
+					<div>
+						<PiNavigationArrowThin
+							size={ 20 }
+							className={ `navigate` }
+						/>
+					</div>
+				</div>
+			</a>
 
-            <div className={ `flex-full-center gap-2` }>
-                <h6>Contact</h6>
-                <div>
-                    <PiNavigationArrowThin
-                    size={20}
-                    className={`navigate`}
-                    />
-                </div>
-            </div>
-
-        </div>
-    );
+		</div>
+	);
 };
