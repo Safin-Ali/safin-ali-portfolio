9eb8affd7078730fd9947aa5685ec2108df322d9
Safin-Ali <safin.ali.7205@gmail.com>
2023-10-11 22:47:04 +0600

    data loading anim raw css replaced with tailwind css amd dark ui

--- src/components/Loading-Anim/Default-Loader.tsx
+++ src/components/Loading-Anim/Default-Loader.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import styleLoader from './loading.style.module.css';
 
 export default function DefaultLoader() {
 
 	return (
-		<div className={`${styleLoader.spinner}`}>
-			<div className={`${styleLoader.dot1}`}></div>
-			<div className={`${styleLoader.dot2}`}></div>
-			<div className={`${styleLoader.dot3}`}></div>
+		<div className={`spinner`}>
+			<div className={`dot1`}></div>
+			<div className={`dot2`}></div>
+			<div className={`dot3`}></div>
 		</div>
 	);
 };
