43eb6586be900c9c7b546d5ca958571a1a272159
Safin Ali <safin.ali.7205@gmail.com>
2024-02-22 02:23:34 +0600

    feat: :sparkles: implemented skills section

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Image from 'next/image';
+import userInterfacePlaceHolder from '../../../public/assets/user_interface_placeholder.webp';
+import serverPlaceHolder from '../../../public/assets/server_placeholder.webp';
+import toolsPlaceHolder from '../../../public/assets/tools_placeholder.webp';
+
+export default function Page() {
+
+	const skillsData = {
+		'front_end': [
+			'ReactJS',
+			'NextJS',
+			'TailwindCSS',
+			'Redux'
+		],
+		'back_end': [
+			'NodeJS',
+			'ExpressJS',
+			'Socket.io',
+			'MongoDB',
+		],
+		'tools': [
+			'VS Code',
+			'TypeScript',
+			'Figma',
+			'GIT'
+		]
+	}
+
+	const renderSkills = () => {
+		return Object.entries(skillsData).map(([category, skills], idx) => {
+			let imgSrc = category === 'front_end' ? userInterfacePlaceHolder : category === 'back_end' ? serverPlaceHolder : toolsPlaceHolder
+			return (
+				<div
+				key={ idx }
+				className={ `border p-5 rounded-xl bg-fade-linear-tb border-primary-border` }
+			>
+				<div>
+					<Image
+						width={300}
+						height={300}
+						alt={'skill_front_end'}
+						src={imgSrc.src}
+					/>
+				</div>
+
+				<h6 className={ `my-2 text-xl font-medium` }><span>{ category.replace('_', ' ') }</span></h6>
+				<ul>
+					{ skills.map((skill, idx) => <li key={ idx }>{ skill }</li>) }
+				</ul>
+			</div>
+			)
+		});
+	}
+
+	return (
+		<div className={ `relative flex-full-center h-screen` }>
+			<div className={`text-center`}>
+				<div className={ `my-5 text-3xl` }>
+					My Technical Skills
+				</div>
+				<div className={ `grid grid-cols-3 capitalize gap-5 justify-center items-center h-full` }>
+					{ renderSkills() }
+				</div>
+			</div>
+		</div>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,18 @@ const config: Config = {
 		extend: {
 			colors:{
 				'navy':'#050a1a',
-				'glow':'#99999e',
+				'glow':'#D9D9D9',
 				'primary-header-txt':'#dbdbdd',
 				'primary-parag-txt':'#a0a1a6',
 				'primary-btn-purple':'#6357f7',
 				'primary-btn-pink':'#a937fe',
 				'primary-border':'#222433'
+			},
+			boxShadow:{
+				'half-border':'0px -0.5px 0px 0.5px'
+			},
+			backgroundImage:{
+				'fade-linear-tb':'linear-gradient(to bottom, rgba(217, 217, 217, 0.15) 0%, transparent 70%, transparent 90%, transparent 100%)'
 			}
 		},
 	},
