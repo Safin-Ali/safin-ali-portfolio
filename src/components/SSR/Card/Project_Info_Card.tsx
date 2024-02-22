b5677223b10a6f53c85c10cc3f486fe53f49f627
Safin Ali <safin.ali.7205@gmail.com>
2024-02-22 21:07:59 +0600

    feat: :sparkles: a new Project_Info_Card component with improved css code structure

---
+++ src/components/SSR/Card/Project_Info_Card.tsx
@@ -0,0 +1,63 @@
+import Text_Expandable_Nav_Btn from '@/components/CSR/Button/Text_Expandable_Nav_Btn';
+import { ProjectDataShortType } from '@/types/types';
+import Image from 'next/image';
+import React from 'react';
+
+export default function Project_Info_Card(props: ProjectDataShortType) {
+
+	const {
+		_id,
+		projectCodeURL,
+		projectLiveURL,
+		projectName,
+		projectThumb,
+		projectUsedLib,
+		shortText
+	} = props;
+
+	return (
+		<figure className={ `project_card_container` }>
+
+			<div className={`basis-1/2`}>
+				<div className={`my-2`}>
+					<h4 className={`text-2xl`}>{ projectName }</h4>
+				</div>
+				<div className={`my-5`}>
+					<p className={`font-light`}>
+						<Text_Expandable_Nav_Btn
+						path={_id}
+						sliceRange={195}
+						className={`text-justify`}
+						>
+							{shortText}
+						</Text_Expandable_Nav_Btn>
+					</p>
+				</div>
+
+				<div className={ `flex gap-2 my-2` }>
+					<div>
+						<a className={ `primary_btn` } href={ projectCodeURL } target={ '_blank' }>
+							<span>Source Code </span>
+							<span className={`animate-slight_to_lr inline-block`}>»</span>
+						</a>
+					</div>
+					<div>
+						<a className={ `primary_btn` } href={ projectLiveURL } target={ '_blank' }>
+							<span>Live Demo </span>
+							<span className={`animate-slight_to_lr inline-block`}>»</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className={`basis-1/2`}>
+				<Image
+					src={projectThumb}
+					alt={projectName+`_thumbnail`}
+					width={300}
+					height={300}
+					className={`rounded-md mx-auto`}
+				/>
+			</div>
+		</figure>
+	);
+}
---
+++ src/types/types.d.ts
@@ -0,0 +1,17 @@
+export interface ProjectDataShortType {
+	_id:string
+	projectName: string;
+	projectThumb: string;
+	projectLiveURL: string;
+	projectCodeURL: string;
+	projectUsedLib: string[];
+	shortText: string;
+};
+export interface ProjectDataType extends ProjectDataShortType {
+	projectOthersInfo: {
+		challenges: string[];
+		features: string[];
+	};
+	projectCategory: string[];
+	projectTech: string[];
+};
