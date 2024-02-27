8090bfaa50560690566018a9cb02090f4dc78915
Safin Ali <safin.ali.7205@gmail.com>
2024-02-27 09:10:06 +0600

    feat: :sparkles: new props once property
    supported single string while once props true and clean sideEffect after unmount

--- src/components/CSR/Multi-Text-Slider/Multi_Text_Slider.tsx
+++ src/components/CSR/Multi-Text-Slider/Multi_Text_Slider.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import styleMultiTextSlide from './multi-text-slide.module.css';
 interface PropsType {
 	textArr: string[],
+	once?:boolean,
 	style?: React.CSSProperties,
 	className?: string,
 	changeTime?: number,
@@ -13,38 +14,50 @@ export default function Multi_Text_Slide(props: PropsType) {
 
 	const { textArr,
 		className = ``,
+		once = false,
 		style = {},
 		changeTime = 2,
 		lazyTime = 0.5
 	} = props;
 
-	const [textIdx, setTextIndex] = useState<number>(0);
+	const [textState, updateTextState] = useState<{
+		index:number,
+		remount:boolean
+	}>({
+		index:0,
+		remount:false
+	});
 
+	if (changeTime <= lazyTime) {
+		throw new Error(`{changeTime} props value must be greater than {lazyTime} props value`);
+	} else if (textArr.length <= 1 && !once) {
+		throw new Error(`{textArr} props must be contain multiple string`);
+	}
 	useEffect(() => {
-
-		if (changeTime <= lazyTime) {
-			throw new Error(`{changeTime} props value must be greater than {lazyTime} props value`);
-		} else if (textArr.length <= 1) {
-			throw new Error(`{textArr} props must be contain multiple string`);
+		if(!once) {
+			setTimeout(() => {
+				if (textState.index === textArr.length - 1) {
+					return updateTextState({...textState,index:0});
+				}
+				updateTextState({...textState,index:textState.index+1});
+			}, changeTime*1000);
+		} else {
+			setTimeout(() => {
+				updateTextState({...textState,remount:!textState.remount});
+			}, changeTime*1000);
 		}
-
-		setTimeout(() => {
-			if (textIdx === textArr.length - 1) {
-				return setTextIndex(0);
-			}
-			setTextIndex(textIdx + 1);
-		}, changeTime*1000);
-	}, [textIdx]);
+		return () => {};
+	}, [textState.index,textState.remount]);
 
 	return (
-		<div className={ styleMultiTextSlide['multi-text-root'] }>
+		<div key={textState.remount ? 'remounted' : 'not-remounted'} className={ styleMultiTextSlide['multi-text-root'] }>
 			{
 				textArr.map((txt, idx) => <span
 					style={ {
 						...style,
 						animationDuration: `${lazyTime + 's'}`
 					} }
-					className={ `${idx === textIdx ? styleMultiTextSlide['multi-text-textIn'] : ''} ${className}`}
+					className={ `${idx === textState.index ? styleMultiTextSlide['multi-text-textIn'] : ''} ${className}`}
 					key={ idx }
 				>
 					{ txt }
