5216f9da4b01eb8781398aece978a134285595c7
Safin Ali <safin.ali.7205@gmail.com>
2024-03-02 07:22:01 +0600

    feat: :sparkles: a new Anchor_CV button for hype cv or resume url

---
+++ src/components/SSR/Anchor/Anchor_CV.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import React from 'react';
+import { MdOutlineFileDownload } from "react-icons/md";
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+	children:string,
+	cdnUrl:string
+}
+
+export default function Anchor_CV(props:Props) {
+
+	const {cdnUrl,children,className,...attr} = props;
+
+	return (
+		<Link
+			href={ cdnUrl }
+			target={ '_blank' }
+			prefetch={ false }
+		>
+			<div
+				className={ `flex items-center w-fit primary_btn gap-2 py-1.5 ${className || ''}` }
+				{...attr}
+			>
+				<div>
+					{children}
+				</div>
+				<div>
+					<MdOutlineFileDownload
+						size={ 25 }
+						className={ `animate-bounce_low` }
+					/>
+				</div>
+			</div>
+		</Link>
+	);
+}
