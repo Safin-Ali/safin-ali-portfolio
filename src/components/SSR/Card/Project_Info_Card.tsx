8d75bae70d4cf3ffa566019117dd910e18b66ff3
Safin Ali <safin.ali.7205@gmail.com>
2024-03-04 10:37:46 +0600

    style: :iphone: projects page responsive for sm and md screen

--- src/components/SSR/Card/Project_Info_Card.tsx
+++ src/components/SSR/Card/Project_Info_Card.tsx
@@ -17,38 +17,38 @@ export default function Project_Info_Card(props: ProjectDataShortType) {
 	return (
 		<figure className={ `project_card_container` }>
 
-			<div className={`basis-1/2`}>
-				<div className={`my-2`}>
+			<div className={`basis-full order-2 md:order-none`}>
+				<div className={`my-1 sm:my-2`}>
 					<h4 className={`text-2xl text-primary-header-txt`}>{ projectName }</h4>
 				</div>
-				<div className={`my-5`}>
+				<div className={`my-3 md:my-5`}>
 					<p className={`font-light`}>
 						<Text_Expandable_Nav_Btn
 						path={_id}
 						sliceRange={200}
-						className={`text-justify`}
+						className={`text-left`}
 						>
 							{shortText}
 						</Text_Expandable_Nav_Btn>
 					</p>
 				</div>
 
-				<div className={ `flex gap-2 my-2` }>
+				<div className={ `flex gap-2 mt-5 sm:mt-2 my-2` }>
 					<div>
-						<a className={ `primary_btn` } href={ projectCodeURL } target={ '_blank' }>
+						<a className={ `primary_btn text-sm sm:text-base` } href={ projectCodeURL } target={ '_blank' }>
 							<span>Source Code </span>
 							<span className={`animate-slight_to_lr inline-block`}>»</span>
 						</a>
 					</div>
 					<div>
-						<a className={ `primary_btn` } href={ projectLiveURL } target={ '_blank' }>
+						<a className={ `primary_btn text-sm sm:text-base` } href={ projectLiveURL } target={ '_blank' }>
 							<span>Live Demo </span>
 							<span className={`animate-slight_to_lr inline-block`}>»</span>
 						</a>
 					</div>
 				</div>
 			</div>
-			<div className={`basis-1/2`}>
+			<div className={`basis-full order-1 md:order-none`}>
 				<Image
 					src={projectThumb}
 					alt={projectName+`_thumbnail`}
