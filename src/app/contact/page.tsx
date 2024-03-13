bb58a91aceb7ad88c5c015c127bf56de5d3549d6
Safin Ali <safin.ali.7205@gmail.com>
2024-03-13 17:48:01 +0600

    style: 📱 using body tag element 100dvh with responsive contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,132 +11,137 @@ import Contact_Card from '@/components/CSR/Card/Contact_Card';
 export default function Page() {
 
 	return (
-		<div className={ `flex-full-center h-[calc(100%-15%)] overflow-y-scroll md:overflow-y-auto md:h-full` }>
+		<div className={ `flex-full-center h-full` }>
 			<div className={ `w-full h-full` }>
 				<div
 					className={ `content_header animate-fade_in_down opacity-0` }
-					style={{
-						animationDelay:'0.2s',
-						animationDuration:'0.5s'
-					}}
+					style={ {
+						animationDelay: '0.2s',
+						animationDuration: '0.5s'
+					} }
 				>
 					Let&apos;s Connect
-					<p><span className={ `text-base font-light` }>For business inquiries or potential collaborations, reach out to me.:</span></p>
+					<p>
+						<span className={`hidden md:inline-block text-base font-light`}>For business inquiries or potential collaborations, </span>
+						<span className={ `text-base font-light` }>reach out to me.:</span>
+					</p>
 				</div>
 
-				<div className={ `flex-full-center h-fit sm:px-[2%] md:px-[5%] lg:px-0 flex-col sm:flex-row md:h-page sm:gap-[5%] my-10` }>
+				<div className={`content_wrapper`}>
+					<div className={ `flex-full-center h-fit sm:px-[2%] md:px-[5%] lg:px-0 flex-col sm:flex-row md:h-page sm:gap-[5%] my-10` }>
 
-					{/* contact via email form */ }
-					<div
-						className={ `basis-full w-[95%] mx-auto md:mx-0 sm:ml-auto md:w-auto animate-fade_in my-5 md:my-0 opacity-0` }
-						style={{
-							animationDuration:'0.5s',
-							animationDelay:'0.3s'
-						}}
-					>
-						<div className={ `w-full lg:w-3/4 xl:w-1/2 mx-auto xl:mx-0 xl:ml-auto` }>
-							<form>
-								<Floating_Input_Field
-									labelVal={ 'Subject' }
-								/>
-								<Floating_Input_Field
-									labelVal={ 'Your Name' }
-								/>
-								<Floating_Input_Field
-									labelVal={ 'Your Email' }
-								/>
-								<Floating_Textarea_Field
-									labelVal={ 'Message' }
-									rows={ 5 }
-									maxLength={ 500 }
-									className={ `resize-none` }
-								/>
-								<div>
-									<Primary_Btn
-										className={ `px-6` }
-									>
-										Sent
-									</Primary_Btn>
-								</div>
-							</form>
-						</div>
-					</div>
-
-					{/* contatct with social */ }
-					<div
-						className={ `basis-full w-[95%] mx-auto md:mx-0 md:w-auto animate-fade_in_up opacity-0` }
-						style={{
-							animationDuration:'0.45s',
-							animationDelay:'0.5s',
-						}}
-					>
-						<div className={ `basis-full flex flex-row md:flex-col justify-center gap-5` }>
-							{/* social block 1  */ }
-							<div className={ `social_contact_card` }>
-
-								<div>
-									<Contact_Card
-										desSrc={ 'https://www.linkedin.com/in/safin-ali/' }
-										iconNode={ <SlSocialLinkedin
-											className={ `mx-auto animate-bounce` } size={ 25 }
-										/>
-										}
-										userId={ `safin-ali` }
+						{/* contact via email form */ }
+						<div
+							className={ `basis-full w-[95%] mx-auto md:mx-0 sm:ml-auto md:w-auto animate-fade_in my-5 md:my-0 opacity-0` }
+							style={ {
+								animationDuration: '0.5s',
+								animationDelay: '0.3s'
+							} }
+						>
+							<div className={ `w-full lg:w-3/4 xl:w-1/2 mx-auto xl:mx-0 xl:ml-auto` }>
+								<form>
+									<Floating_Input_Field
+										labelVal={ 'Subject' }
 									/>
-								</div>
-								<div>
-									<Contact_Card
-										desSrc={ 'https://www.linkedin.com/in/safin-ali/' }
-										iconNode={ <RiTwitterXLine
-											className={ `mx-auto animate-bounce` } size={ 25 }
-										/>
-										}
-										userId={ `safin-ali` }
+									<Floating_Input_Field
+										labelVal={ 'Your Name' }
 									/>
-								</div>
-								<div>
-									<Contact_Card
-										desSrc={ 'https://web.whatsapp.com/send?phone=+8801316987205' }
-										iconNode={ <SiMedium
-											className={ `mx-auto animate-bounce` } size={ 30 }
-										/>
-										}
-										userId={ `01316987205` }
+									<Floating_Input_Field
+										labelVal={ 'Your Email' }
 									/>
-								</div>
+									<Floating_Textarea_Field
+										labelVal={ 'Message' }
+										rows={ 5 }
+										maxLength={ 500 }
+										className={ `resize-none` }
+									/>
+									<div>
+										<Primary_Btn
+											className={ `px-6` }
+										>
+											Sent
+										</Primary_Btn>
+									</div>
+								</form>
 							</div>
+						</div>
 
-							{/* social block 2 */ }
-							<div className={ `social_contact_card social_contact_block_end` }>
-								<div>
-									<Contact_Card
-										desSrc={ 'http://m.me/safin.ali.7205' }
-										iconNode={ <SlSocialInstagram
-											className={ `mx-auto animate-bounce` } size={ 30 }
+						{/* contatct with social */ }
+						<div
+							className={ `basis-full w-[95%] mx-auto md:mx-0 md:w-auto animate-fade_in_up opacity-0` }
+							style={ {
+								animationDuration: '0.45s',
+								animationDelay: '0.5s',
+							} }
+						>
+							<div className={ `basis-full flex flex-row md:flex-col justify-center gap-5` }>
+								{/* social block 1  */ }
+								<div className={ `social_contact_card` }>
+
+									<div>
+										<Contact_Card
+											desSrc={ 'https://www.linkedin.com/in/safin-ali/' }
+											iconNode={ <SlSocialLinkedin
+												className={ `mx-auto animate-bounce` } size={ 25 }
+											/>
+											}
+											userId={ `safin-ali` }
 										/>
-										}
-										userId={ `safin.ali.7205` }
-									/>
+									</div>
+									<div>
+										<Contact_Card
+											desSrc={ 'https://www.linkedin.com/in/safin-ali/' }
+											iconNode={ <RiTwitterXLine
+												className={ `mx-auto animate-bounce` } size={ 25 }
+											/>
+											}
+											userId={ `safin-ali` }
+										/>
+									</div>
+									<div>
+										<Contact_Card
+											desSrc={ 'https://web.whatsapp.com/send?phone=+8801316987205' }
+											iconNode={ <SiMedium
+												className={ `mx-auto animate-bounce` } size={ 30 }
+											/>
+											}
+											userId={ `01316987205` }
+										/>
+									</div>
 								</div>
-								<div>
-									<Contact_Card
-										desSrc={ 'http://m.me/safin.ali.7205' }
-										iconNode={ <PiMessengerLogoLight
-											className={ `mx-auto animate-bounce` } size={ 30 }
+
+								{/* social block 2 */ }
+								<div className={ `social_contact_card social_contact_block_end` }>
+									<div>
+										<Contact_Card
+											desSrc={ 'http://m.me/safin.ali.7205' }
+											iconNode={ <SlSocialInstagram
+												className={ `mx-auto animate-bounce` } size={ 30 }
+											/>
+											}
+											userId={ `safin.ali.7205` }
 										/>
-										}
-										userId={ `safin.ali.7205` }
-									/>
-								</div>
-								<div>
-									<Contact_Card
-										desSrc={ 'https://web.whatsapp.com/send?phone=+8801316987205' }
-										iconNode={ <PiWhatsappLogoLight
-											className={ `mx-auto animate-bounce` } size={ 30 }
+									</div>
+									<div>
+										<Contact_Card
+											desSrc={ 'http://m.me/safin.ali.7205' }
+											iconNode={ <PiMessengerLogoLight
+												className={ `mx-auto animate-bounce` } size={ 30 }
+											/>
+											}
+											userId={ `safin.ali.7205` }
 										/>
-										}
-										userId={ `01316987205` }
-									/>
+									</div>
+									<div>
+										<Contact_Card
+											desSrc={ 'https://web.whatsapp.com/send?phone=+8801316987205' }
+											iconNode={ <PiWhatsappLogoLight
+												className={ `mx-auto animate-bounce` } size={ 30 }
+											/>
+											}
+											userId={ `01316987205` }
+										/>
+									</div>
 								</div>
 							</div>
 						</div>
