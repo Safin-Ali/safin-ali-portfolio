434b14690424cf38a4a2af3fb4f560386d2bea43
Safin Ali <safin.ali.7205@gmail.com>
2024-03-16 15:40:35 +0600

    fix: :lipstick: fixed modal content blury

--- src/components/CSR/Modal/Intercept_Modal.tsx
+++ src/components/CSR/Modal/Intercept_Modal.tsx
@@ -1,17 +1,16 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { IoClose } from "react-icons/io5";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement>{
 	children: React.ReactNode,
-	header:string
 }
 
 export default function Intercept_Modal(props: Props) {
 
-	const { children,header, className, ...attr } = props;
+	const { children, className, ...attr } = props;
 
 	const [modalState,setModalState] = useState<boolean>(false);
 
@@ -33,15 +32,18 @@ export default function Intercept_Modal(props: Props) {
 	}, [])
 
 	return createPortal(<>
-		<div className={ `modal_container` }>
-			<div
-				className={ `modal_content ${modalState && 'animate-fade_out_down'} ${className || ''}` }
+		<div className={ `modal_blur` }></div>
+		<div
+				className={ `modal_container ${modalState ? 'animate-fade_out_down' :''} ${className || ''}` }
+				style={{
+					animationDuration:'0.275s'
+				}}
 				onAnimationEnd={() => modalState && back()}
 				{...attr}
 			>
 					<div className={`border-b border-primary-border w-full px-5 py-2 top-0 flex justify-between`}>
 					<div className={`pt-1 text-lg md:text-2xl font-semibold`}>
-						<h4>{header}</h4>
+
 					</div>
 					<button onClick={dismissModal}>
 						<IoClose
@@ -50,10 +52,9 @@ export default function Intercept_Modal(props: Props) {
 						/>
 					</button>
 				</div>
-				<div className={`w-full top-[15%] px-5 overflow-y-auto`}>
+				<div className={`w-full overflow-y-auto`}>
 					{children}
 				</div>
-			</div>
 		</div>
 	</>, document.getElementById('modal-root')!);
 }
